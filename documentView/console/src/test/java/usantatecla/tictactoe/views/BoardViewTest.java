619677f06c728f937452a93a4413f6d44ccd9a13
Pablo <pabloguijarro96@gmail.com>
2021-01-15 16:15:27 +0100

    [dv.console] Fix BoardViewTest

--- documentView/console/src/test/java/usantatecla/tictactoe/views/BoardViewTest.java
+++ documentView/console/src/test/java/usantatecla/tictactoe/views/BoardViewTest.java
@@ -1,24 +1,23 @@
 package usantatecla.tictactoe.views;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.OngoingStubbing;
 import usantatecla.tictactoe.models.*;
 import usantatecla.tictactoe.types.Color;
 import usantatecla.utils.Console;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.regex.Pattern;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
-import static org.mockito.MockitoAnnotations.openMocks;
 
 @ExtendWith(MockitoExtension.class)
 public class BoardViewTest {
+
+    private static final int BOARD_DIMENSION = Coordinate.DIMENSION * Coordinate.DIMENSION;
 
     @Mock
     private Game game;
@@ -26,84 +25,69 @@ public class BoardViewTest {
     @InjectMocks
     private BoardView boardView;
 
-    @BeforeEach
-    void before() {
-        openMocks(this);
-    }
-
     @Test
-    public void testGivenCompleteBoardWhenWriteThenPrintInCorrectOrder() { //TODO Reducir tamanyo
+    public void testGivenCompleteBoardWhenWriteThenPrintInCorrectOrder() {
         Console console = mock(Console.class);
         try (MockedStatic<Console> staticConsole = mockStatic(Console.class)) {
             staticConsole.when(Console::getInstance).thenReturn(console);
             InOrder colorPrinted = inOrder(console);
-            String[] board = {
-                    "X X",
-                    "XO ",
+            Color[] colors = this.getBoardColors(
+                    "X X"+
+                    "XO "+
                     "O O"
-            };
-            Color[] colors = this.getBoardColors(board);
-            when(this.game.getColor(any(Coordinate.class))).thenReturn(
-                    colors[0],colors[1],colors[2],
-                    colors[3],colors[4],colors[5],
-                    colors[6],colors[7],colors[8]);
+            );
+            OngoingStubbing<Color> stubbing = when(this.game.getColor(any(Coordinate.class)));
+            for(int i = 0; i< colors.length; i++){
+                stubbing = stubbing.thenReturn(colors[i]);
+            }
             this.boardView.write();
             verify(console, times(2)).writeln("---------------");
-            verify(console, times(Coordinate.DIMENSION*Coordinate.DIMENSION + Coordinate.DIMENSION)).write(" | ");
+            verify(console, times(BoardViewTest.BOARD_DIMENSION + Coordinate.DIMENSION)).write(" | ");
             verify(console, times(Coordinate.DIMENSION)).writeln();
             for(int i=0;i<colors.length;i++){
-                if(colors[i]==Color.X){
-                    colorPrinted.verify(console).write("X");
-                }else if(colors[i]==Color.O){
-                    colorPrinted.verify(console).write("O");
-                }else{
-                    colorPrinted.verify(console).write(" ");
-                }
+                colorPrinted.verify(console).write(this.colorToString(colors[i]));
             }
         }
     }
 
     @Test
-    public void testGivenEmptyBoardWhenWriteThenPrint() { //TODO CON O SIN USAR METODO PRIVADO
+    public void testGivenEmptyBoardWhenWriteThenPrint() {
         Console console = mock(Console.class);
         try (MockedStatic<Console> staticConsole = mockStatic(Console.class)) {
             staticConsole.when(Console::getInstance).thenReturn(console);
-            /*Board board = new BoardBuilder().rows(
-                    "   ",
-                    "   ",
-                    "   ").build();*/
             when(this.game.getColor(any(Coordinate.class))).thenReturn(Color.NULL);
             this.boardView.write();
             verify(console, times(2)).writeln("---------------");
-            verify(console, times(Coordinate.DIMENSION*Coordinate.DIMENSION + Coordinate.DIMENSION)).write(" | ");
+            verify(console, times(BoardViewTest.BOARD_DIMENSION + Coordinate.DIMENSION)).write(" | ");
             verify(console, times(Coordinate.DIMENSION)).writeln();
-            verify(console, times(Coordinate.DIMENSION*Coordinate.DIMENSION)).write(" ");
+            verify(console, times(BoardViewTest.BOARD_DIMENSION)).write(" ");
         }
     }
 
-    private Color[] getBoardColors(String[] board){ //TODO REDUCIR TAMANYO
-        assert board.length == 3;
-        List<String> strings = new ArrayList<>();
-        Color[] colors = new Color[Coordinate.DIMENSION*Coordinate.DIMENSION];
-        for (String row : board) {
-            assert Pattern.matches("[XO ]{3}", row);
-            strings.add(row);
-        }
-        int cont=0;
-        for (int i = 0; i < Coordinate.DIMENSION; i++) {
-            for (int j = 0; j < Coordinate.DIMENSION; j++) {
-                if(strings.get(i).charAt(j) == 'X'){
-                    colors[cont] = Color.X;
-                }else if(strings.get(i).charAt(j) == 'O'){
-                    colors[cont] = Color.O;
-                }else{
-                    colors[cont] = Color.NULL;
-                }
-                cont++;
+    private Color[] getBoardColors(String board){
+        assert board.length() == BoardViewTest.BOARD_DIMENSION;
+        assert Pattern.matches("[XO ]{9}", board);
+        Color[] colors = new Color[BoardViewTest.BOARD_DIMENSION];
+        for (int i = 0; i < BoardViewTest.BOARD_DIMENSION; i++) {
+            if(board.charAt(i) == 'X'){
+                colors[i] = Color.X;
+            }else if(board.charAt(i) == 'O'){
+                colors[i] = Color.O;
+            }else{
+                colors[i] = Color.NULL;
             }
         }
         return colors;
     }
 
+    private String colorToString(Color color){ //TODO VER SI PONER EN EL MODELO DE COLOR
+        if(color==Color.X){
+            return "X";
+        }else if(color==Color.O){
+            return "O";
+        }else{
+            return " ";
+        }
+    }
 
 }
